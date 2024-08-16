38ca8af06ce88a14e4f459da5f7760f99b3d84d3
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-08-16 10:02:45 +0700

    update

--- Lab-5/src/App.vue
+++ Lab-5/src/App.vue
@@ -42,6 +42,7 @@
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import { useRouter, useRoute } from 'vue-router'
--- Lab-5/src/main.ts
+++ Lab-5/src/main.ts
@@ -2,6 +2,7 @@ import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
